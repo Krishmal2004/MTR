53cb3010cad1edefe0abcc346969f75e2eec248c
krishmal2004 <krishmaldinidu5466@gmail.com>
2026-09-07 00:24:59 +0530

    Embed Windows metadata to prevent false positive AV detections

---
+++ tui-runner/build.rs
@@ -0,0 +1,11 @@
+﻿fn main() {
+    #[cfg(windows)]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set("ProductName", "TUI Runner");
+        res.set("FileDescription", "TUI Runner Application");
+        res.set("LegalCopyright", "Copyright (c) 2024");
+        res.set("InternalName", "tui-runner.exe");
+        res.compile().unwrap();
+    }
+}
